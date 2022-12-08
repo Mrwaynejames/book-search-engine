b41d58caf4b90a81015c4af2f9a7d9b79c9c21c0
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-12-08 14:50:16 -0800

    resolvers syntax error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,22 +4,22 @@ const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
     Query:{
-       person async (parent, args, context) => {
+       me: async (parent, args, context) => {
         if (context.user) {
             const userData = await User.findOne({})
             .select('-__v -password')
-                .populate('books')
+            .populate('books')
             
                 return userData;
         }
        } 
-    }
+    },
 
 
     Mutation: {
-        CreateUser: = async (parent,args) => {
-            const user =await User.create(args):
-            const token = singToken(user):
+        CreateUser: async (parent,args) => {
+            const user =await User.create(args);
+            const token = singleToken(user);
 
             return {token,user};
         },
@@ -37,18 +37,18 @@ const resolvers = {
     return {token,user};
     },
 
-    saveBook async (parent, args, context) => {
+    saveBook: async (parent, args, context) => {
         if (contect.user) {
             const updateUser = await User.findOneAndUpdate(
                 {_id: context.user._id},
-                {$addToSet: {savedBooks: args.input}}
+                {$addToSet: {savedBooks: args.input}},
                 {new: true}
             );
             return updateUser;
         }
         throw new AuthenticationError('log in first');
     },
-    deleteBook async (parent, args, context) => {
+    deleteBook: async (parent, args, context) => {
         if(context.user) {
             const updatedUser = await User.findOneAndUpdate(
                 { _id: context.user._id },
