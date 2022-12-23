55f82a90a5025ab77842b3a0c25b5867a69e4100
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-12-23 11:14:07 -0800

    auth check

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,11 +5,6 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
 
   authMiddleware: function({req}) {
 
@@ -32,10 +27,9 @@ module.exports = {
     }
     return req;
   },
-};
-//   signToken: function ({ username, email, _id }) {
-//     const payload = { username, email, _id };
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
 
-//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-//   },
-// };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
